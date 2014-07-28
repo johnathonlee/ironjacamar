c47d81403191dea0ad37769e0e49dd4fb91c4dbf
jesperpedersen <jesper.pedersen@ironjacamar.org>
2014-07-28 11:04:29 -0400

    [JBJCA-1199] Guard against ISE in TransactionSynchronizer

--- core/src/main/java/org/jboss/jca/core/connectionmanager/transaction/TransactionSynchronizer.java
+++ core/src/main/java/org/jboss/jca/core/connectionmanager/transaction/TransactionSynchronizer.java
@@ -57,6 +57,9 @@ public class TransactionSynchronizer implements Synchronization
    /** The logger */
    private static CoreLogger log = Logger.getMessageLogger(CoreLogger.class, TransactionSynchronizer.class.getName());
 
+   /** Trace */
+   private static boolean trace = log.isTraceEnabled();
+
    /** The records */
    private static ConcurrentMap<Transaction, Record> records =
       new ConcurrentHashMap<Transaction, Record>();
@@ -156,6 +159,9 @@ public class TransactionSynchronizer implements Synchronization
     */
    public synchronized boolean removeEnlisted(Synchronization synch)
    {
+      if (enlisted == null)
+         return false;
+
       return enlisted.remove(synch);
    }
    
@@ -198,13 +204,39 @@ public class TransactionSynchronizer implements Synchronization
          if (record == null)
          {
             record = newRecord;
-            if (tsr != null)
+
+            if (trace)
+               log.tracef("Adding: %s [%s]", System.identityHashCode(tx), tx.toString());
+
+            try
             {
-               tsr.registerInterposedSynchronization(record.getTransactionSynchronizer());
+               if (tsr != null)
+               {
+                  tsr.registerInterposedSynchronization(record.getTransactionSynchronizer());
+               }
+               else
+               {
+                  tx.registerSynchronization(record.getTransactionSynchronizer());
+               }
             }
-            else
+            catch (Throwable t)
             {
-               tx.registerSynchronization(record.getTransactionSynchronizer());
+               records.remove(tx);
+
+               if (t instanceof SystemException)
+               {
+                  throw (SystemException)t;
+               }
+               else if (t instanceof RollbackException)
+               {
+                  throw (RollbackException)t;
+               }
+               else
+               {
+                  SystemException se = new SystemException(t.getMessage());
+                  se.initCause(t);
+                  throw se;
+               }
             }
          }
       }
@@ -262,13 +294,39 @@ public class TransactionSynchronizer implements Synchronization
          if (record == null)
          {
             record = newRecord;
-            if (tsr != null)
+
+            if (trace)
+               log.tracef("Adding: %s [%s]", System.identityHashCode(tx), tx.toString());
+
+            try
             {
-               tsr.registerInterposedSynchronization(record.getTransactionSynchronizer());
+               if (tsr != null)
+               {
+                  tsr.registerInterposedSynchronization(record.getTransactionSynchronizer());
+               }
+               else
+               {
+                  tx.registerSynchronization(record.getTransactionSynchronizer());
+               }
             }
-            else
+            catch (Throwable t)
             {
-               tx.registerSynchronization(record.getTransactionSynchronizer());
+               records.remove(tx);
+
+               if (t instanceof SystemException)
+               {
+                  throw (SystemException)t;
+               }
+               else if (t instanceof RollbackException)
+               {
+                  throw (RollbackException)t;
+               }
+               else
+               {
+                  SystemException se = new SystemException(t.getMessage());
+                  se.initCause(t);
+                  throw se;
+               }
             }
          }
       }
@@ -335,18 +393,37 @@ public class TransactionSynchronizer implements Synchronization
          invokeAfter(ccmSynch, status);  
       }
 
-      // Cleanup the maps -- only trust TransactionSynchronizer
-      boolean found = false;
-
-      Iterator<Map.Entry<Transaction, Record>> iterator = records.entrySet().iterator();
-      while (!found && iterator.hasNext())
+      // Try with hashCode first
+      if (records.remove(tx) == null)
       {
-         Map.Entry<Transaction, Record> next = iterator.next();
-         if (next.getValue().getTransactionSynchronizer().equals(this))
+         // Cleanup the maps -- only trust TransactionSynchronizer
+         boolean found = false;
+
+         Iterator<Map.Entry<Transaction, Record>> iterator = records.entrySet().iterator();
+         while (!found && iterator.hasNext())
          {
-            iterator.remove();
-            found = true;
+            Map.Entry<Transaction, Record> next = iterator.next();
+            if (next.getValue().getTransactionSynchronizer().equals(this))
+            {
+               iterator.remove();
+               found = true;
+            }
          }
+
+         if (found)
+         {
+            if (trace)
+               log.tracef("Removed: %s [%s]", System.identityHashCode(tx), tx.toString());
+         }
+         else
+         {
+            log.transactionNotFound(tx);
+         }
+      }
+      else
+      {
+         if (trace)
+            log.tracef("Removed: %s [%s]", System.identityHashCode(tx), tx.toString());
       }
    }
 
